77f4fd238a58a880bfa7f84b96ee891ee5e30bd5
migbaranes <migbaranes@gmail.com>
2023-12-01 23:01:31 +0300

    CsvFileSource

--- src/test/java/ru/netology/services/MonthsRestTest.java
+++ src/test/java/ru/netology/services/MonthsRestTest.java
@@ -3,30 +3,33 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class MonthsRestTest {
 
-    @Test
+    @ParameterizedTest
 
-    public void testCorrespondsNumberMonths(){
+    @CsvFileSource(files = "src/test/resources/NumberMonths.csv")
+    public void testCorrespondsNumberMonths(int expected, int income, int expenses, int threshold) {
+
         MonthsRest service = new MonthsRest();
-        int expected = 2;
-        int actual = service.calcMonths(100000, 60000, 150000);
+//        int expected = 2;
+        int actual = service.calcMonths(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
     }
 
 
-    @Test
-
-    public void testCorrespondsNumberMonthsSmallIncome(){
-        MonthsRest service = new MonthsRest();
-        int expected = 3;
-        int actual = service.calcMonths(10000, 3000, 20000);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-    }
+//    @Test
+//
+//    public void testCorrespondsNumberMonthsSmallIncome(){
+//        MonthsRest service = new MonthsRest();
+//        int expected = 3;
+//        int actual = service.calcMonths(10000, 3000, 20000);
+//
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
+}
